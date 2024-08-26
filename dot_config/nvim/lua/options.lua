bd59590276070c27ef38cf7851fcfcd9af746692
Bartosz Rudnicki <brudnicki@gmv.com>
2024-08-26 17:56:50 +0200

    add options to init

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -6,10 +6,12 @@ local g = vim.g
 -- g.toggle_theme_icon = "   "
 
 -------------------------------------- options ------------------------------------------
-
 o.number = true
 o.wrap = false
 o.wrapscan = false -- search don't wrap
+o.ignorecase = true -- search ignore case
+o.colorcolumn = 80  -- draw char limit line 
+o.relativenumber = true -- line numbers relative to the current one
 
 -------------------------------------- cmds exec ------------------------------------------
 
