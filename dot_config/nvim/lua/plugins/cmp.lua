90e40f2a4ba956ca14333a4eb1db8bfb2322fd39
Bartosz Rudnicki <bartek@rudnicki.szczecin.pl>
2025-04-12 22:10:07 +0200

    theme, clang lsp, clang format

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -59,37 +59,5 @@ return {
 
         }) -- end of setuo()
 
-        -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-        -- Set configuration for specific filetype.
-        --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]] --
-
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        -- cmp.setup.cmdline({ '/', '?' }, {
-        --   mapping = cmp.mapping.preset.cmdline(),
-        --   sources = {
-        --     { name = 'buffer' }
-        --   }
-        -- })
-        --
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        -- cmp.setup.cmdline(':', {
-        --   mapping = cmp.mapping.preset.cmdline(),
-        --   sources = cmp.config.sources({
-        --     { name = 'path' }
-        --   }, {
-        --     { name = 'cmdline' }
-        --   }),
-        --   matching = { disallow_symbol_nonprefix_matching = false }
-        --
-        --
-        --   })
-        --
-    end
+   end
 }
--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -2,15 +2,18 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     dependencies = {
---        "williamboman/mason.nvim",
---        "williamboman/mason-lspconfig.nvim",
+        --        "williamboman/mason.nvim",
+        --        "williamboman/mason-lspconfig.nvim",
     },
     config = function()
-        require("lspconfig").clangd.setup{
-            cmd = {"clangd", "--enable-config", "--log=error"} -- error/info/verbose
+        require("lspconfig").clangd.setup {
+            cmd = { "clangd", "--enable-config", "--log=error" } -- error/info/verbose
         }
-    -- require'lspconfig'.pylyzer.setup{}
-    require'lspconfig'.pyright.setup{}
-    require'lspconfig'.lua_ls.setup{}
+        -- require'lspconfig'.pylyzer.setup{}
+        require 'lspconfig'.pyright.setup {}
+        require 'lspconfig'.lua_ls.setup {}
+        require 'lspconfig'.texlab.setup {}
+        require 'lspconfig'.cmake.setup {}
+
     end,
 }
