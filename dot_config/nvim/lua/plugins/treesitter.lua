75a326ed78cb8e41c06d8eabd4226ea2a2618874
Bartosz Rudnicki <bartek@rudnicki.szczecin.pl>
2026-01-17 14:54:29 +0100

    add indenations

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "cpp", "python", "cmake", "make", "bash" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "cpp", "python", "cmake", "make", "bash", "vhdl"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
